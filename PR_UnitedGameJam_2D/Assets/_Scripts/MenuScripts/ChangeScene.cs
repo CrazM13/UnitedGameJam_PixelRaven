92f8482d5eeb81bbbc3a5b0a1258a21548239545
Rui Santos <cardkingrui@gmail.com>
2020-06-27 14:29:16 -0400

    Added Scene Management

---
+++ PR_UnitedGameJam_2D/Assets/_Scripts/MenuScripts/ChangeScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int sceneChangeNumber;
+    private int sceneCount = SceneManager.sceneCountInBuildSettings;
+
+    public void ChangeToLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ChangeToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ChangeToAnyScene()
+    {
+        SceneManager.LoadScene(sceneChangeNumber);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoseScreen()
+    {
+        SceneManager.LoadScene(sceneCount - 1);
+    }
+
+    public void WinScreen()
+    {
+        SceneManager.LoadScene(sceneCount - 2);
+    }
+}
