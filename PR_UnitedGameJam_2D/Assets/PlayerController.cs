2e4d4f17467644e864fdaff4889ff042d59b16e0
Stephen Brown <clank4491@gmail.com>
2020-06-27 20:00:28 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ PR_UnitedGameJam_2D/Assets/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    float jumpHeight = 700 ;
+   [SerializeField] private bool grounded = true;
+    private BoxCollider2D boxCollider;
+
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+        moveVelocity = moveInput.normalized * speed;
+        if (grounded)
+        {
+           // moveVelocity.y = 0;
+
+            if (Input.GetAxis("Jump") >= .8f)
+            {
+                // Calculate the velocity required to achieve the target jump height.
+                moveVelocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+                grounded = false;
+            }
+                       
+        }
+
+        moveVelocity.y += Physics2D.gravity.y * Time.deltaTime;
+        transform.Translate(moveVelocity * Time.deltaTime);
+
+
+
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+
+        foreach (Collider2D hit in hits)
+        {
+            // Ignore our own collider.
+            if (hit == boxCollider) 
+                continue;
+
+            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+
+                if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && moveVelocity.y < 0)
+                {
+                    grounded = true;
+                }
+            }
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);       
+    }
+
+
+}
